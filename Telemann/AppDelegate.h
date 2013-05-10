a3313f5cc918db16c65fd31eeb80737ad8259ee2
Paul Stefan Ort <mail@paulstefanort.com>
2013-05-09 23:36:34 -0500

    Add Xcode project.

---
+++ Telemann/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Telemann
+//
+//  Created by Paul Stefan Ort on 5/9/13.
+//  Copyright (c) 2013 Paul Stefan Ort. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Telemann/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Telemann
+//
+//  Created by Paul Stefan Ort on 5/9/13.
+//  Copyright (c) 2013 Paul Stefan Ort. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
